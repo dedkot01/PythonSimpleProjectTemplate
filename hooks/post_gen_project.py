4427f1f55ec0ec35a42d2da9f84f298a8362423e
dedkot <dedkot01@gmail.com>
2025-04-23 09:45:44 +0300

    add src layout support

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -6,6 +6,13 @@ def init_git() -> None:
     subprocess.run(["git", "init", "-q"])
 
 
+def create_src_layout() -> None:
+    src_folder = Path("src")
+    src_folder.mkdir(exist_ok=True)
+    package_folder = Path("{{ cookiecutter.__project_slug }}")
+    package_folder.rename(src_folder.joinpath(package_folder))
+
+
 def init_venv() -> None:
     command = ["uv", "sync", "-q", "-p", "{{ cookiecutter.python_version }}"]
     if {{ cookiecutter.create_polygon }} is True:
@@ -14,6 +21,8 @@ def init_venv() -> None:
 
 
 def main() -> None:
+    if {{ cookiecutter.create_src_layout }} is True:
+        create_src_layout()
     if {{ cookiecutter.create_polygon }} is False:
         polygon_file = Path("polygon.ipynb")
         polygon_file.unlink(missing_ok=True)
